5f35515008aaa4291b4bf281a11e83cdcf6ce81c
RayRafa Leal <rayaneribeirofe@gail.com>
2023-10-18 21:43:35 -0400

    primeiro teste

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8"/>
+	<title> Netflix </title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge, chromer=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Site piloto Netflix"/>
+	<meta name="keywords" content="Netflix da Ray">
+	<meta name="autor" content="Rayane Ribeiro">
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
+	<link rel="stylesheet" href="css/folha_all.css">
+	<link rel="stylesheet" href="css/home.css">
+	
+	
+</head>
+<body>
+	<header>
+		<h1> Netflix </h1>
+		<img src="images/.jpg"
+	</header>
+	<nav>
+		<h1> Seja bem vindo ao Netflix</h1>
+		<p>
+			<a href="#"> Entrar</a>
+			<a href="#"> Casdastrar</a>
+		</p>
+	</nav>
+	<main>
+		<h1>Se divirta de onde estiver</h1>
+		<hr>
+		<p> Esse site foi criado pra quem não paga netflix, mas gosta de usar o usuário dos coleguinhas</p>
+		<p> não seja um parasita Rafael, pague sua própria netflix </p>
+	</main>
+	<footer>
+	<hr>
+		<p> Site desenvolvido por <a herf="https://www.instagram.com/ray_hi1/"> Rayane Ribeiro</a></p>
+	</footer>
+	
+	</body>
+</html>
